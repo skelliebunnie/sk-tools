da057eaff1c5df4e733f6feaa6ec2537590efd99
SkellieBunnie <pandahart@gmail.com>
2019-11-19 08:46:19 -0800

    removed the profile photo functions from skb-admin.functions

--- admin/skb-admin.functions.php
+++ admin/skb-admin.functions.php
@@ -28,33 +28,3 @@ function skb_image_upload($target, $url='', $setting = false) {
 
   return "<div id='{$target}_image-upload'>". $image_link ."</div>";
 }
-
-function skb_user_profile_photo( $user ) { 
-	wp_enqueue_media();
-	wp_enqueue_script('skb-misha-script');
-
-	$user = wp_get_current_user();
-	$user_meta = get_user_meta($user->ID);
-	$avatar = get_avatar($user->ID);
-	var_dump($avatar);
-	?>
-  <h3><?php _e("Profile Photo", "skb_admin"); ?></h3>
-
-  <table class="form-table">
-	  <tr>
-	    <th><label for="avatar"><?php _e("Profile Photo", "skb_domain"); ?></label></th>
-	    <td>
-	      <?php echo skb_image_upload('avatar', '', false); ?>
-	    </td>
-	  </tr>
-  </table>
-<?php }
-add_action( 'show_user_profile', 'skb_user_profile_photo' );
-add_action( 'edit_user_profile', 'skb_user_profile_photo' );
-
-function save_extra_user_profile_fields( $user_id ) {
-  if ( !current_user_can( 'edit_user', $user_id ) ) { 
-      return false; 
-  }
-  update_user_meta( $user_id, 'avatar', $_POST['avatar'] );
-}
