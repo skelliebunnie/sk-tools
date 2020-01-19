6d100045c82a1849b4298e3a981d30f4211a925c
Margaret Ralston <pandahart@gmail.com>
2020-01-19 14:43:04 -0800

    add: logic for modifying dates / times in the sk-datetime tool. format is same as date->modify()

--- sk-datetime/sk.datetime.php
+++ sk-datetime/sk.datetime.php
@@ -19,13 +19,20 @@ function sk_datetime_shortcode($atts) {
 			'separator'				=> '&nbsp;',
 			'bold_date'				=> 'false',
 			'bold_time'				=> 'false',
-			'bold_all'				=> 'false'
+			'bold_all'				=> 'false',
+			'modify'					=> '0'
 		), $atts );
 
 		$order = explode(",", $a['order']);
 
 		$date_format = $sk_options['sk-dt-default_date_format'];
 		$time_format = $sk_options['sk-dt-default_time_format'];
+
+		$today = date($date_format);
+		$now = date($time_format);
+
+		$date = $a['date']; $time = $a['time'];
+		$dateTime_date = new DateTime; $dateTime_time = new DateTime;
 
 		if($a['date_format'] !== 'default' && $a['date_format'] != $date_format) {
 			$date_format = $a['date_format'];
@@ -34,11 +41,6 @@ function sk_datetime_shortcode($atts) {
 		if($a['time_format'] !== 'default' && $a['time_format'] != $time_format) {
 			$time_format = $a['time_format'];
 		}
-
-		$today = date($date_format);
-		$now = date($time_format);
-
-		$date = $a['date']; $time = $a['time'];
 
 		if($a['date'] != 'today' && $a['date'] != 'false') {
 			$date = date($date_format, strtotime($a['date']));
@@ -59,6 +61,35 @@ function sk_datetime_shortcode($atts) {
 
 		} else {
 			$time = NULL;
+		}
+
+		$full_datetime = $a['time'] != "false" ? $date ." ". $time : null;
+
+		if( $a['modify'] != "0" ) {
+			$dt_date = null; $dt_time = null;
+			if($a['date'] === 'today') {
+				$dt_date = date('Y-m-d');
+			} elseif($a['date'] != 'false') {
+				$dt_date = date('Y-m-d', strtotime($a['date']));
+			} else {
+				$dt_date = "1970-01-01";
+			}
+
+			if($a['date'] === 'today') {
+				$dt_time = date('H:i:s');
+			} elseif($a['time'] != 'false') {
+				$dt_time = date('H:i:s', strtotime($a['time']));
+			} else {
+				$dt_time = "00:00:00";
+			}
+
+			$full_datetime = "{$dt_date}T{$dt_date}Z";
+
+			$dt = new DateTime( strtotime($full_datetime) );
+			$dt_date = $dateTime_date->modify("{$a['modify']}");
+			$dt_time = $dateTime_time->modify("{$a['modify']}");
+			$date = $dt_date->format($date_format);
+			$time = $dt_time->format($time_format);
 		}
 
 		$separator = $a['separator'];
