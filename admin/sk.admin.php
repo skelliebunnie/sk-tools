8d1bb18040507cd9d930ec82bf1cbc6aa2e63f93
Margaret Ralston <pandahart@gmail.com>
2020-06-04 10:21:00 -0700

    removed some stuff from the admin page?

--- admin/sk.admin.php
+++ admin/sk.admin.php
@@ -1,15 +1,14 @@
 <?php
 
 function sk_admin() {
+	global $wpdb;
 	wp_enqueue_style('sk-admin-styles');
 
 	wp_enqueue_script('sk-admin-script', SK_ROOTURL ."/admin/js/admin.js", array('jquery'), null, true);
 
 	ob_start();
 
-	echo "<h1>SK Tools</h1>";
-?>
-This is the admin page for SK-TOOLS
-<?php
+	echo "<h1>SK Tools</h1><p>Documentation / admin page</p>";
+
 	echo ob_get_clean();
 }
